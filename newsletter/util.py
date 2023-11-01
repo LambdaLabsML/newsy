3aeb5e4a22589b029424de03361679003cf9d5cf
Corey Lowman <clowman@lambdal.com>
2023-11-01 13:51:54 -0400

    Remove accept encoding

--- newsletter/util.py
+++ newsletter/util.py
@@ -20,7 +20,6 @@ def get_details_from_url(url):
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
             "Referer": "https://www.google.com",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "en-gb, en",
         },
     )
