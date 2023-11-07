0b924730df100fbeda67a48f8bc1f39782e056b2
Corey Lowman <clowman@lambdal.com>
2023-11-07 15:02:14 -0500

    Using langchain to gain access to langsmith

--- newsletter/lm.py
+++ newsletter/lm.py
@@ -1,14 +1,11 @@
 import openai
+from langchain.chat_models import ChatOpenAI
 
 
 def summarize_post(title, content, model="gpt-3.5-turbo-16k"):
-    result = openai.ChatCompletion.create(
-        timeout=10,
-        model=model,
-        messages=[
-            {
-                "role": "system",
-                "content": f"""{title}
+    llm = ChatOpenAI(model=model, request_timeout=10)
+    result = llm.invoke(
+        f"""{title}
 
 {content}
 ---
@@ -20,21 +17,15 @@ Generate a bulleted list that summarizes the main points from the above article.
 4. Write the summary as if a 5 year old was the reader.
 Generate at most 3 bullet points.
 Here is the bulleted list summary:
-""",
-            }
-        ],
+"""
     )
-    return result.choices[0].message["content"]
+    return result.content
 
 
 def summarize_abstract(title, content, model="gpt-3.5-turbo-16k"):
-    result = openai.ChatCompletion.create(
-        timeout=10,
-        model=model,
-        messages=[
-            {
-                "role": "system",
-                "content": f"""{title}
+    llm = ChatOpenAI(model=model, request_timeout=10)
+    result = llm.invoke(
+        f"""{title}
 
 {content}
 ---
@@ -53,21 +44,15 @@ The format of the summary must look like the following:
 ```
 
 Here is the summary:
-""",
-            }
-        ],
+"""
     )
-    return result.choices[0].message["content"]
+    return result.content
 
 
 def summarize_comment(title, summary, comment, model="gpt-3.5-turbo-16k"):
-    result = openai.ChatCompletion.create(
-        timeout=10,
-        model=model,
-        messages=[
-            {
-                "role": "system",
-                "content": f"""**Title**: {title}
+    llm = ChatOpenAI(model=model, request_timeout=10)
+    result = llm.invoke(
+        f"""**Title**: {title}
 
 **Article Summary**:
 {summary}
@@ -76,29 +61,21 @@ def summarize_comment(title, summary, comment, model="gpt-3.5-turbo-16k"):
 {comment}
 
 Write an extremely short (less than 5 words; no need for grammatically correct) info bite summarizing the comment:
-""",
-            }
-        ],
+"""
     )
-    return result.choices[0].message["content"]
+    return result.content
 
 
 def matches_filter(summary, filter, model="gpt-3.5-turbo-16k"):
-    result = openai.ChatCompletion.create(
-        timeout=10,
-        model=model,
-        messages=[
-            {
-                "role": "system",
-                "content": f"""**Article**: {summary}
+    llm = ChatOpenAI(model=model, request_timeout=10)
+    result = llm.invoke(
+        f"""**Article**: {summary}
 
 We are looking for Articles that match the following **Filter**: {filter}
 
 Does the above Article match the above Filter? The Answer should be Yes or No:
 **Answer**:
-""",
-            }
-        ],
+"""
     )
-    content = result.choices[0].message["content"].strip().lower()
+    content = result.content.strip().lower()
     return content == "yes" or content not in ["yes", "no"]
